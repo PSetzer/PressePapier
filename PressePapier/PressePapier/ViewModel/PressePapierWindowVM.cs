4d617010448fa2a281a3fed1b725db20a38207a1
Paul Setzer <paul.setzer@free.fr>
2016-02-16 00:59:27 +0100

    ajout viewmodel avec propriétés

---
+++ PressePapier/PressePapier/ViewModel/PressePapierWindowVM.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PressePapier.ViewModel
+{
+    class PressePapierWindowVM : ObservableObject
+    {
+        private string _textTB1;
+        public string TextTB1
+        {
+            get { return _textTB1; }
+            set
+            {
+                _textTB1 = value;
+                OnPropertyChanged(TextTB1);
+            }
+        }
+
+        private string _textTB2;
+        public string TextTB2
+        {
+            get { return _textTB2; }
+            set
+            {
+                _textTB2 = value;
+                OnPropertyChanged(TextTB2);
+            }
+        }
+
+        private string _textTB3;
+        public string TextTB3
+        {
+            get { return _textTB3; }
+            set
+            {
+                _textTB3 = value;
+                OnPropertyChanged(TextTB3);
+            }
+        }
+
+        private string _textTB4;
+        public string TextTB4
+        {
+            get { return _textTB4; }
+            set
+            {
+                _textTB4 = value;
+                OnPropertyChanged(TextTB4);
+            }
+        }
+
+        private string _textTB5;
+        public string TextTB5
+        {
+            get { return _textTB5; }
+            set
+            {
+                _textTB5 = value;
+                OnPropertyChanged(TextTB5);
+            }
+        }
+
+        private string _textTB6;
+        public string TextTB6
+        {
+            get { return _textTB6; }
+            set
+            {
+                _textTB6 = value;
+                OnPropertyChanged(TextTB6);
+            }
+        }
+
+        private string _textTB7;
+        public string TextTB7
+        {
+            get { return _textTB7; }
+            set
+            {
+                _textTB7 = value;
+                OnPropertyChanged(TextTB7);
+            }
+        }
+
+        private string _textTB8;
+        public string TextTB8
+        {
+            get { return _textTB8; }
+            set
+            {
+                _textTB8 = value;
+                OnPropertyChanged(TextTB8);
+            }
+        }
+
+        private string _textTB9;
+        public string TextTB9
+        {
+            get { return _textTB9; }
+            set
+            {
+                _textTB9 = value;
+                OnPropertyChanged(TextTB9);
+            }
+        }
+
+        private string _textTB10;
+        public string TextTB10
+        {
+            get { return _textTB10; }
+            set
+            {
+                _textTB10 = value;
+                OnPropertyChanged(TextTB10);
+            }
+        }
+
+        private string _textFichierEnCours;
+        public string TextFichierEnCours
+        {
+            get { return _textFichierEnCours; }
+            set
+            {
+                _textFichierEnCours = value;
+                OnPropertyChanged(TextFichierEnCours);
+            }
+        }
+
+        private string _textEnregEffectue;
+        public string TextEnregEffectue
+        {
+            get { return _textEnregEffectue; }
+            set
+            {
+                _textEnregEffectue = value;
+                OnPropertyChanged(TextEnregEffectue);
+            }
+        }
+
+        
+
+    }
+}
